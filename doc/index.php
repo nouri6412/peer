173fe798a459c0ee3d1bb5a8d6049aff67de6d82
Jalil Nouri <nouri.6412@gmail.com>
2022-10-04 13:45:49 +0330

    init

---
+++ doc/index.php
@@ -0,0 +1,120 @@
+<html>
+
+<head>
+  <script src="peerjs.min.js"></script>
+  <!-- <script src="https://unpkg.com/peerjs@1.3.2/dist/peerjs.min.js"></script> -->
+
+  <script src="jquery.min.js"></script>
+</head>
+
+<body>
+  <div>
+    <input id="cid" name="cid" value="" placeholder="my id" />
+    <br>
+    <input id="did" name="did" value="" placeholder="dest id" />
+    <br>
+    <button onclick="start_connection()">Start connection</button>
+    <br>
+    <textarea id="mes-box" name="mes-box"></textarea>
+    <button onclick="send_message()">send message</button>
+    <br>
+    <div id="res-box"></div>
+    <button onclick="call()">Call</button>
+    <video width="300" height="225" id="my-vid-box" autoplay></video>
+    <div></div>
+    <video width="300" height="225" id="vid-box" autoplay></video>
+  </div>
+  <script>
+    function getRndInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    var peer = new Peer(getRndInteger(1, 1000));
+    peer.on('open', function(id) {
+      console.log('My peer ID is: ' + id);
+      $('#cid').val(id);
+    });
+
+    let conn;
+
+    function start_connection() {
+      conn = peer.connect($('#did').val());
+      conn.on("open", () => {
+        conn.send("hi!");
+      });
+    }
+
+    function send_message() {
+      conn.send($('#mes-box').val());
+    }
+
+    peer.on("connection", (conn1) => {
+      console.log(conn1);
+      conn1.on("data", (data) => {
+        // Will print 'hi!'
+        console.log(data);
+       $('#res-box').append(data);
+      });
+    });
+
+
+    const video = document.getElementById('my-vid-box');
+    video.autoplay = true;
+
+
+    const remote_video = document.getElementById('vid-box');
+    remote_video.autoplay = true;
+
+    const stream_1=null;
+
+    function call() {
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true
+        }).then(function(stream) {
+         // stream_1=stream;
+          video.srcObject = stream;
+          //video.play();
+          const call = peer.call($('#did').val(), stream);
+
+
+          call.on("stream", (stream) => {
+            video.srcObject = stream;
+            //video.play();
+            // Show stream in some <video> element.
+          });
+        });
+      }
+    }
+
+    peer.on("call", (call) => {
+      console.log('calling');
+      // call.answer(video.srcObject); 
+      // call.on("stream", (remoteStream) => {
+      //   console.log('calling');
+      //  // video.play();
+      //   remote_video.srcObject = remoteStream;
+        
+      //  // remote_video.play();
+   
+      //   // Show stream in some <video> element.
+      // });
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+      }).then(function(stream) {
+        call.answer(stream); // Answer the call with an A/V stream.
+        call.on("stream", (remoteStream) => {
+          remote_video.srcObject = remoteStream;
+          remote_video.play();
+          // Show stream in some <video> element.
+        });
+      });
+      }
+    });
+  </script>
+</body>
+
+</html>
---
+++ footer.php
@@ -0,0 +1,9 @@
+
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.5.1.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-select.min.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/Mh1PersianDatePicker.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js" type="text/javascript"></script>
+</body>
+
+</html>
