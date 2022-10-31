7eaeeafb58993cfcb47ed31ce80d5e8b84241c9b
Jalil Nouri <nouri.6412@gmail.com>
2022-10-31 11:27:28 +0330

    wwww

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
-
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.5.1.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-select.min.js" type="text/javascript"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/Mh1PersianDatePicker.js" type="text/javascript"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js" type="text/javascript"></script>
+<!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.5.1.min.js"></script> -->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/datastore.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/date-utils.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/script.js"></script>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -4,10 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/respansive.css">
-    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap-rtl.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- <link href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap-rtl.css" rel="stylesheet" type="text/css" /> -->
     <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" type="text/css" />
 
     <?php wp_head(); ?>
