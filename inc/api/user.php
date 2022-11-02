2aed8fe0a73ecefae537498487774b43b2919867
Jalil Nouri <nouri.6412@gmail.com>
2022-11-02 11:31:32 +0330

    jjj

--- inc/api/user.php
+++ inc/api/user.php
@@ -1,9 +1,9 @@
 <?php
+return;
 class WhatsMessApiUser
 {
     public function __construct()
     {
-        add_action('template_redirect', [$this,'whatsmess_login']);
     }
     public function login()
     {
@@ -12,7 +12,6 @@ class WhatsMessApiUser
         # Don't do anything unless this is an AJAX request
         $method = $wp_query->get(WhatsMess_FILE_Rewrite);
         if (!$method) {
-            wp_send_json_success([], 200);
             return;
         }
 
@@ -34,14 +33,73 @@ class WhatsMessApiUser
 
             wp_send_json_success($array, 200);
             //wp_send_json_error
+        } else {
+            $array = [];
+            wp_send_json_error($array, 200);
         }
-        else
-        {
-            $array = [
-            ];
+    }
+
+    public function signup()
+    {
+        global $wp_query;
+
+        # Don't do anything unless this is an AJAX request
+        $method = $wp_query->get(WhatsMess_FILE_Rewrite);
+        if (!$method) {
+            return;
+        }
+
+        # Check method name
+        if ($method == "whatsmess_signup") {
+            $username = "";
+            $password = "";
+            $email = "";
+
+            if (!empty($_POST['username'])) {
+                $username = wp_unslash($_POST['username']);
+            }
+
+            if (!empty($_POST['password'])) {
+                $password = wp_unslash($_POST['password']);
+            }
+
+            if (!empty($_POST['email'])) {
+                $email = wp_unslash($_POST['email']);
+            }
+
+
+            if (username_exists($username)) {
+                $array = ["error" => __("username already registered", "whatsmess")];
+            } else {
+                $user_id = wp_create_user($username, $password, $email);
+
+                if (is_numeric($user_id) && $user_id > 0) {
+                    $array = ["user_id" => $user_id];
+                } else {
+                    $array = ["error" => __("try again", "whatsmess")];
+                }
+            }
+
+            wp_send_json_success($array, 200);
+            //wp_send_json_error
+        } else {
+            $array = [];
             wp_send_json_error($array, 200);
         }
     }
 }
 
-$WhatsMessApiUser=new WhatsMessApiUser;
+add_action('template_redirect', 'whatsmess_login');
+add_action('template_redirect', 'whatsmess_signup');
+
+function whatsmess_login()
+{
+    $WhatsMessApiUser = new WhatsMessApiUser;
+    $WhatsMessApiUser->login();
+}
+
+function whatsmess_signup()
+{
+    $WhatsMessApiUser = new WhatsMessApiUser;
+    $WhatsMessApiUser->signup();
+}
